e24eff1789f3d7ea38a5a5c98064744fa1e50cb2
Azime Tolumoğlu <azimet@bilge.com.tr>
2025-05-26 12:43:45 +0300

    uı güncelleme yapıldı.

--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -117,43 +117,55 @@
     </v-row>
 
     <v-row justify="center" class="pa-8">
-      <v-col cols="12" md="10" lg="8" class="mb-6">
-        <v-card hover style="min-height: 55vh; width: 100%; position: relative;">
+    <v-col cols="12" md="10" lg="8" class="mb-6">
+      <v-card hover style="min-height: 55vh; width: 100%; position: relative;">
+        
+        <!-- Butonlar yan yana grup halinde -->
+        <div style="position: absolute; top: 10px; right: 10px; display: flex; gap: 8px;">
           <v-btn
             small
             icon
             color="blue"
-            style="position: absolute; top: 10px; right: 10px;"
             @click="toggleEdit('skillset')"
           >
             <v-icon>mdi-pencil</v-icon>
           </v-btn>
+          <v-btn
+            small
+            icon
+            color="red"
+            @click="deleteCard('education', index)"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </div>
 
-          <v-card-text>
-            <template v-if="skillset.isEditing">
-              <v-text-field label="Title" v-model="skillset.title" dense></v-text-field>
-              <v-text-field label="Subtitle" v-model="skillset.subtitle" dense></v-text-field>
-              <v-textarea label="Description" v-model="skillset.description" rows="6" dense></v-textarea>
-              <v-row justify="end" class="mt-2">
-                <v-btn color="success" @click="saveEdit('skillset')">Kaydet</v-btn>
-                <v-btn color="error" class="ml-2" @click="cancelEdit('skillset')">İptal</v-btn>
-              </v-row>
-            </template>
-            <template v-else>
-              <v-row>
-                <v-col cols="12" md="6">
-                  <v-card-title class="text-h6">{{ skillset.title }}</v-card-title>
-                  <v-card-subtitle class="text-subtitle-1">{{ skillset.subtitle }}</v-card-subtitle>
-                </v-col>
-                <v-col cols="12" md="6" class="d-flex align-center">
-                  <p>{{ skillset.description }}</p>
-                </v-col>
-              </v-row>
-            </template>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+        <v-card-text>
+          <template v-if="skillset.isEditing">
+            <v-text-field label="Title" v-model="skillset.title" dense></v-text-field>
+            <v-text-field label="Subtitle" v-model="skillset.subtitle" dense></v-text-field>
+            <v-textarea label="Description" v-model="skillset.description" rows="6" dense></v-textarea>
+            <v-row justify="end" class="mt-2">
+              <v-btn color="success" @click="saveEdit('skillset')">Kaydet</v-btn>
+              <v-btn color="error" class="ml-2" @click="cancelEdit('skillset')">İptal</v-btn>
+            </v-row>
+          </template>
+          <template v-else>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-card-title class="text-h6">{{ skillset.title }}</v-card-title>
+                <v-card-subtitle class="text-subtitle-1">{{ skillset.subtitle }}</v-card-subtitle>
+              </v-col>
+              <v-col cols="12" md="6" class="d-flex align-center">
+                <p>{{ skillset.description }}</p>
+              </v-col>
+            </v-row>
+          </template>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
   </v-container>
 </template>
 
